e8c811ebac56871d86544828563ffda20bdcd524
Max Lamb <lordmaxlamb@gmail.com>
2020-04-01 19:53:47 -0700

    very brain dead

--- app2.js
+++ app2.js
@@ -1,17 +1,26 @@
-import randomThrow from './get-random-throw.js';
 import checkResult from './check-result.js';
-import getRandomThrow from './get-random-throw.js';
+import getRandomThrow from '../get-random-throw.js';
 
 const winsTotal = document.getElementById('winTotal');
 const totalsTotal = document.getElementById('totalTotal');
 const lossesTotal = document.getElementById('lossTotal');
-let button = document.getElementById('RPS');
+let button = document.getElementById('play-button');
 
 let wins = 0;
 let total = 0;
+let losses = 0;
 
 button.addEventListener('click'), () => {
+    const games = document.querySelector ('input:checked');
+    let playerChoice = games.value;
+    console.log(playerChoice);
     let computerThrow = getRandomThrow();
-    const playerChoice = document.querySelector('input:checked');
     checkResult(playerChoice, computerThrow);
-};
+    if (checkResult(playerChoice, computerThrow) === 'win') {
+        wins++;
+        total++;
+    } else if (checkResult(playerChoice, computerThrow) === 'lose') {
+            losses++;
+            total++;
+        } else  {
+        total++};
